899d24ff28417a45f243f7eb379212eb24c815a5
Luan Campos <luanlcampos15@gmail.com>
2021-03-03 22:38:40 -0500

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import MyNav from "./Navbar/Navbar";
 //Custom CSS
 import "./App.css";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 function App() {
   return (
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { Jumbotron, Nav, Navbar } from "react-bootstrap";
 
